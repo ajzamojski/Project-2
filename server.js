3236e576b457072a13ed3990efbf4ea07c190652
Adam Zamojski <adamzamojski@Adams-MacBook-Pro.local>
2017-05-03 20:04:57 -0400

    adding server.js file

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
-// Sets up the Express App
-var app = express();
+
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
 var db = require("./models");
 
-// Sets up the Express app to handle data parsing
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+// Set express
+var app = express();
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Static directory
-app.use(express.static("./public"));
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
 
-// Routes =============================================================
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-require("./routes/html-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
-require("./routes/author-api-routes.js")(app);
+// Import routes and give the server access to them.
+require("./routes/api-routes.js")(app);
+// app.use("/", routes);
 
-// Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
-
